d7193b3b73bcf64b57523449ec4d4d969970b41e
Adrian <adriancosta1215@gmail.com>
2022-04-16 10:52:44 -0300

    Lista de users online e barra lateral implementados

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ const telaInicio = `<div class="tela-inicio">
 
 
 
-
 const corpoCod = `<div class="barra topo">
 <img src="imagens/logo 1.png" class="logo">
 <ion-icon name="people" class="icon" onclick="aparecer()"></ion-icon>
 </div>
 <div class="mensagens-agrupadas">
 </div>
-<div class="fundo-preto hidden" >
-<div class="barra-lateral">
+<div class="fundo-preto hidden" onclick="aparecer()">
+</div>
+<div class="barra-lateral hidden">
 <div class = "lateral-topo">
-    <ion-icon name="close-sharp" class="sair" onclick="aparecer()"></ion-icon>
     <p>Escolha um contato pra enviar mensagem:</p>
 </div>
 <div class="sessao pessoa" onclick="marcar(this)">
@@ -26,15 +25,8 @@ const corpoCod = `<div class="barra topo">
     <p>Todos</p>
     <ion-icon name="checkmark" class="certin hidden"></ion-icon>
 </div>
-<div class="sessao pessoa" onclick="marcar(this)">
-    <ion-icon name="person-circle" class="icon-lat"></ion-icon>
-    <p>Jao</p>
-    <ion-icon name="checkmark" class="certin hidden"></ion-icon>
-</div>
-<div class="sessao pessoa" onclick="marcar(this)">
-    <ion-icon name="person-circle" class="icon-lat"></ion-icon>
-    <p class="">July</p>
-    <ion-icon name="checkmark" class="certin hidden"></ion-icon>
+<div class = "users">
+
 </div>
 <div class="lateral-topo">
     <p>Escolha a visibilidade:</p>
@@ -50,7 +42,7 @@ const corpoCod = `<div class="barra topo">
     <ion-icon name="checkmark" class="certin hidden"></ion-icon>
 </div>
 </div>
-</div>
+
 <div class="barra baixo">
 <div>
     <input type="text" class="digitar" placeholder="Escreva aqui...">
@@ -63,36 +55,45 @@ const corpoCod = `<div class="barra topo">
 
 window.onload = imprimirTela()
 
+let userSelecionado
 let nome 
 let user = {}
+let mensagemEnviar = {
+    from: 0,
+    to: 0,
+    text: 0,
+    type: 0,
+}
 
 function aparecer(){
-    let elemento = document.querySelector(".fundo-preto")
-    console.log(elemento)
-    elemento.classList.toggle("hidden") 
+    const fundoPreto = document.querySelector(".fundo-preto")
+    const barraLateral = document.querySelector(".barra-lateral")
+    console.log(fundoPreto)
+    fundoPreto.classList.toggle("hidden")
+    barraLateral.classList.toggle("hidden") 
 }
 
 function marcar(elementoClicado){
-    
+    userSelecionado = elementoClicado
     console.log(elementoClicado)
     const selecionado = document.querySelector(".sessao.pessoa.selecionado")
     const selecionado2 = document.querySelector(".sessao.visibilidade.selecionado")
 
-    if (selecionado !== null && elementoClicado.classList.contains("pessoa")){
+    if (selecionado !== null && userSelecionado.classList.contains("pessoa")){
         selecionado.querySelector(".certin").classList.add("hidden")
         selecionado.classList.remove("selecionado")
     }
 
     
 
-    if (selecionado2 !== null && elementoClicado.classList.contains("visibilidade")){
+    if (selecionado2 !== null && userSelecionado.classList.contains("visibilidade")){
         selecionado2.querySelector(".certin").classList.add("hidden")
         selecionado2.classList.remove("selecionado")
     }
    
     
-    elementoClicado.classList.add("selecionado")
-    elementoClicado.querySelector(".certin").classList.remove("hidden")
+    userSelecionado.classList.add("selecionado")
+    userSelecionado.querySelector(".certin").classList.remove("hidden")
     
 }
 
@@ -100,6 +101,7 @@ function marcar(elementoClicado){
 function entrarUser(){
     
     nome = document.querySelector(".entrar").value
+
 
     user = { 
         name: nome,
@@ -109,6 +111,10 @@ function entrarUser(){
 
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user)
 
+
+    console.log(user)
+
+
     requisicao.then(tratarSucesso);
     requisicao.catch(tratarErro);
     
@@ -117,12 +123,15 @@ function entrarUser(){
         document.querySelector(".tela-inicio").classList.add("hidden")
         abrirChat()
         const mensagemEntrada = `<div class="mensagem entrada-saida">
-        <p class="horario texto">(09:22:48)</p>
+        <p class="horario texto">()</p>
         <p class = "texto" class="texto"> <strong>${nome}</strong> entra </p>
         </div>`
         espaçoDasMensagens = document.querySelector(".mensagens-agrupadas")
         espaçoDasMensagens.innerHTML += mensagemEntrada
         manterConexao()
+
+        usersON()
+
     }
 
     function tratarErro(erro) {
@@ -148,17 +157,28 @@ function abrirChat(){
 
 function testarMsg(){
     const espaçoDasMensagens = document.querySelector(".mensagens-agrupadas")
-    let mensagemm = document.querySelector(".digitar").value
+    mensagemEnviar.text = document.querySelector(".digitar").value
+    mensagemEnviar.from = nome
+    mensagemEnviar.to = userSelecionado.querySelector(".nomeUser").innerHTML
+    console.log(mensagemEnviar)
     const message = `<div class="mensagem entrada-saida">
-    <p class="horario texto">(09:22:48)</p>
-    <p class = "texto"> ${mensagemm} </p>
+    <p class="horario texto">()</p>
+    <p class = "texto"> ${mensagemEnviar.text} </p>
     </div>`
     espaçoDasMensagens.innerHTML += message
 
-    
 
-    
+}
 
+
+
+function entradas(){
+
+
+   
+    
+   
+   
 }
 
 let requisicao2
@@ -184,4 +204,45 @@ function manterConexao(){
         clearInterval(meuInterval)
     }
 }
+
+let responseData
+
+function usersON(){
+
+    let participantes = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+    .then(function (response){
+        setInterval(usersOnline, 10000)
+    })
+
+    function usersOnline(){
+        axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+        .then(function (response){
+        responseData = response.data
+        console.log(responseData.length)
+        
+        let lugarOn = document.querySelector(".users")
+        lugarOn.innerHTML = ""
+        
+        for (let i = 0; i < responseData.length; i++){
+            const moldeContato = `<div class="sessao pessoa" onclick="marcar(this)">
+            <ion-icon name="person-circle" class="icon-lat"></ion-icon>
+            <p class = "nomeUser">${responseData[i].name}</p>
+            <ion-icon name="checkmark" class="certin hidden"></ion-icon>
+            </div>`
+            lugarOn.innerHTML += moldeContato
+        }
+        })
+    }
+
+    const mensagemEntrada = `<div class="mensagem entrada-saida">
+        <p class="horario texto">()</p>
+        <p class = "texto" class="texto"> <strong>${nome}</strong> entra </p>
+        </div>`
+
+}
+
+function getMessages(){
+    axios.get()
+}
+
 
